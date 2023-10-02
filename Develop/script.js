c54e53e95d67907e4537a384f96cd3fa311b300a
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-10-02 17:13:28 -0400

    added catch statements to switch, setting hour<9 to all future state and after 5 to past state

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ const timeDisplayEl = $('#current_time')
 function displayTime() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm a');
   timeDisplayEl.text(rightNow);
- }
+}
 
 // "setInterval" runs at a 500ms interval for smoother time transitions on the page.
 setInterval(displayTime(), 500);
@@ -34,6 +34,18 @@ setInterval(function currentHour() {
 
   let currentTime = dayjs().hour();
   switch (currentTime) {
+    case (currentTime < 9):
+      hr9el.addClass('future')
+      hr10el.addClass('future')
+      hr11el.addClass('future')
+      hr12el.addClass('future')
+      hr13el.addClass('future')
+      hr14el.addClass('future')
+      hr15el.addClass('future')
+      hr16el.addClass('future')
+      hr17el.addClass('future')
+      break;
+
     case 9:
       console.log('hour 9')
       hr9el.addClass('present')
@@ -139,7 +151,7 @@ setInterval(function currentHour() {
       hr17el.addClass('future')
       break;
 
-    case 16:
+    case 17:
       console.log('hour 17')
       hr9el.addClass('past')
       hr10el.addClass('past')
@@ -151,16 +163,28 @@ setInterval(function currentHour() {
       hr16el.addClass('past')
       hr17el.addClass('present')
       break;
+
+    default:
+      hr9el.addClass('past')
+      hr10el.addClass('past')
+      hr11el.addClass('past')
+      hr12el.addClass('past')
+      hr13el.addClass('past')
+      hr14el.addClass('past')
+      hr15el.addClass('past')
+      hr16el.addClass('past')
+      hr17el.addClass('past')
+
   }
   console.log(currentTime)
 
-},1000);
+}, 1000);
 
 
 // pulling the currently saved to-do object from local storage
-function retrieveToDos(){
+function retrieveToDos() {
   let currentToDos = JSON.parse(localStorage.getItem('todoItemsArray'))
-  }
+}
 
 
 
@@ -173,18 +197,19 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-    // TODO: Add code to get any user input that was saved in localStorage and set
+  // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-
+  let hourEl = 3
+  let textInput = 0
   const todoItem = {
 
     hour: hourEl,
     text: textInput
-  
+
   }
-  
+
   localStorage.setItem("todoItemsArray", JSON.stringify(todoItem))
 
 });
@@ -194,21 +219,21 @@ $(function () {
 
 
 // pulling the currently saved to-do object from local storage
-function retrieveToDos(){
-let currentToDos = JSON.parse(localStorage.getItem('todoItemsArray'))
+function retrieveToDos() {
+  let currentToDos = JSON.parse(localStorage.getItem('todoItemsArray'))
 }
 
 
 // creating the object to be stored and turing it into a JSON file 
 function storeToDos() {
-let hourEl = 3
-let textInput = 0
-const todoItem = {
+  let hourEl = 3
+  let textInput = 0
+  const todoItem = {
 
-  hour: hourEl,
-  text: textInput
+    hour: hourEl,
+    text: textInput
 
-}
+  }
 
-localStorage.setItem("todoItemsArray", JSON.stringify(todoItem))
+  localStorage.setItem("todoItemsArray", JSON.stringify(todoItem))
 }
